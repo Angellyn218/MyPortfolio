f4eef3b5b3f47eeebeb5ec503b5336a7f6cce44f
Angellyn218 <40581490+Angellyn218@users.noreply.github.com>
2018-07-10 15:19:26 -0700

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,27 @@
+//hide and show section on a website
+
+var recentProjects = document.getElementById("projects");
+
+/*function show(){
+  if(recentProjects.style.display === "none"){
+    recentProjects.style.display = "block";
+  } else {
+    recentProjects.style.display = "none";
+  }
+}
+
+function foundDude(){
+  if ()
+}
+*/
+/*function changeColor(){
+  recentProjects.style.color = "blue"
+}
+
+function changeBack(){
+  recentProjects.style.color = "green"
+}
+
+recentProjects.onmouseover = changeColor;
+recentProjects.onmouseout = changeBack;
+*/
